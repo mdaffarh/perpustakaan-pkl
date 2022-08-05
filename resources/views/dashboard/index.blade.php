c3ac72b8eaae45402a97725de2f6a94927703f09
AntsF25 <raisupardi56@smk.belajar.id>
2022-08-05 07:05:54 +0700

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,26 @@
+@extends('layout.main')
+@section('title', "Perpustakaan")
+
+@section('right')
+@include('layout.right')
+@endsection
+
+@section('left')
+@include('layout.left')
+@endsection
+
+@section('content')
+@include('layout.content')
+@endsection
+
+@section('footer')
+@include('layout.footer')
+@endsection
+
+
+{{-- 
 <h1>Halo {{ auth()->user()->name }}</h1>
 <form method="POST" action="/logout">
     @csrf
     <button type="submit" value="Logout">Logout</button>
-</form>
+</form> --}}
